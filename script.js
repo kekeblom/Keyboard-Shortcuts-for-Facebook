c08634e2d1eaeb9841ae7d0cfd4b7ed6d15839cd
Kenneth Blomqvist <kekeblom@gmail.com>
2012-07-09 15:20:47 +0300

    Changed to use Mousetrap, updated jquery

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-    var k = new Kibo();
     var cur = -1;
     var blueBarHeight = $('#blueBar').height(); // floated bar
     var isFocus = function(){
@@ -14,26 +13,22 @@ $(document).ready(function(){
         }
         $(window).scrollTop($li.offset().top-blueBarHeight);
     };
-    k.up(['j', 'k', 'u'], function(){
-        if (isFocus()) {
-            return true;
-        }
-        switch (k.lastKey()) {
-            case 'j':
-                cur++;
-                break;
-            case 'k':
-                if (cur <= 0) {
-                    return;
-                }
-                cur--;
-                break;
-            case 'u':
-                cur = 0;
-                break;
-            default:
-                break;
-        }
+
+    // bindings
+
+    Mousetrap.bind('j', function(){
+        cur++;
         doScroll();
+        return; 
+    });
+    Mousetrap.bind('k', function(){
+        cur--;
+        doScroll();
+        return; 
+    });
+    Mousetrap.bind('u', function(){
+        cur = 0;
+        doScroll();
+        return; 
     });
 });
