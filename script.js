e6b78a858f128d86569cea60e1a24f9089386892
Kenneth Blomqvist <kekeblom@gmail.com>
2012-07-10 14:03:28 +0300

    smooth scrolling, added shortcuts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
     var cur = -1;
+    var $html = $('html, body');
     var blueBarHeight = $('#blueBar').height(); // floated bar
     var isFocus = function(){
         return $('input,textarea').is(':focus');
@@ -11,24 +12,36 @@ $(document).ready(function(){
             cur = 0;
             return;
         }
-        $(window).scrollTop($li.offset().top-blueBarHeight);
+        $html.stop().animate({
+            scrollTop: $li.offset().top - blueBarHeight
+        }, 75);
+        // $(document).scrollTop($li.offset().top-blueBarHeight);
     };
 
     // bindings
 
-    Mousetrap.bind('j', function(){
+    Mousetrap.bind('j', function(){         // scroll down one item
         cur++;
         doScroll();
         return; 
     });
-    Mousetrap.bind('k', function(){
+    Mousetrap.bind('k', function(){         // scroll up one item
         cur--;
         doScroll();
         return; 
     });
-    Mousetrap.bind('u', function(){
+    Mousetrap.bind('u', function(){         // scroll to top
         cur = 0;
         doScroll();
         return; 
     });
+    Mousetrap.bind('g f', function(){       // go to feed on 'g f'
+        window.open("/", "_self");
+    })
+    Mousetrap.bind('g m', function(){       // go to messages on 'g m'
+        window.open("/messages/", "_self");
+        return;
+    });
+    Mousetrap.bind('g e', function() { window.open("/events/", "_self") }); // go to events on 'g e'
+
 });
